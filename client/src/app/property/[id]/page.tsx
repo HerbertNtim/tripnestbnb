1ec0fad410004a87a9a40275d0cb6293b28a866e
HerbertNtim <herbertntim15@gmail.com>
2025-10-24 10:02:41 +0000

    property detail page implemented

--- client/src/app/property/[id]/page.tsx
+++ client/src/app/property/[id]/page.tsx
@@ -1,14 +1,15 @@
+import ReservationSidebar from '@/components/ReservationSidebar'
 import Image from 'next/image'
 import React from 'react'
 
 const PropertyDetails = () => {
   return (
-    <main className='max-w-[1500px] mx-auto px-6'>
-      <div className='w-full h-[64vh] overflow-hidden rounded-xl relative'>
+    <main className='max-w-[1500px] mx-auto px-6 pb-6'>
+      <div className='w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative'>
         <Image fill src={'/beach_1.jpg'} alt='Beach' className='object-cover w-full h-full' />
       </div>
 
-      <div className='mt-4 grid grid-cols-1 md:grid-cols-5 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-5 gap-4'>
         <div className='col-span-3 py-6 pr-6'>
           <h1 className='mb-4 text-4xl'>Property Name</h1>
 
@@ -29,7 +30,7 @@ const PropertyDetails = () => {
           <p className='mt-6 text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum odit facilis laudantium repellat. Maiores minima eligendi nisi consectetur quidem a accusamus incidunt vel necessitatibus, commodi asperiores. Vero dolores tenetur rem?</p>
         </div>
         
-        <div>right</div>
+        <ReservationSidebar />
       </div>
     </main>
   )
